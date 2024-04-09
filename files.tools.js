669edb84f3b31167d114e538e6975b66f1b774c2
morgan denis <morgan.denis.60@gmail.com>
2024-04-09 22:57:10 +0200

    feat: add main system

---
+++ files.tools.js
@@ -0,0 +1,64 @@
+const fs = require('fs');
+const events = require('events');
+const { createReadStream } = require('fs');
+const readline = require('readline');
+
+class FileReader {
+    constructor(path) {
+        this.readStream = createReadStream(path);
+    }
+
+    async start(fn) {
+        try {
+            const rl = readline.createInterface({
+                input: this.readStream,
+                crlfDelay: Infinity
+            });
+            let lineNumber = 0;
+            rl.on('line', (line) => {
+                fn(line, lineNumber);
+                lineNumber += 1;
+            });
+
+            await events.once(rl, 'close');
+
+            console.log('Reading file line by line with readline done.');
+            const used = process.memoryUsage().heapUsed / 1024 / 1024;
+            console.log(`The script uses approximately ${Math.round(used * 100) / 100} MB`);
+        } catch (err) {
+            console.error(err);
+        }
+    }
+}
+
+class FileWriter {
+    constructor(filename) {
+        this._filename = filename;
+        if (!this._filename) {
+            throw new Error('filename is required for creating a file');
+        }
+        if (fs.existsSync(this._filename)) {
+            fs.unlinkSync(this._filename);
+        }
+        this.writeStream = fs.createWriteStream(this._filename, { flags: 'a' });
+    }
+
+    insertLines(lines) {
+        lines.map((line) => this.insertLine(line));
+    }
+
+    insertLine(line) {
+        if (line) {
+            this.writeStream.write(`${line}\n`, 'utf-8');
+        }
+    }
+
+    close() {
+        this.writeStream.end();
+    }
+}
+
+module.exports = {
+    FileReader,
+    FileWriter
+};
---
+++ index.js
@@ -0,0 +1,33 @@
+function computeType(type) {
+    switch (type) {
+        case "CARD_PAYMENT":
+            return 6;
+        case "TRANSFER":
+            return 4;
+        case "EXCHANGE":
+        case "TOPUP":
+            return 0;
+        default:
+            console.log('unknowk type', type);
+            return 0;
+    }
+}
+(async function () {
+    const { FileReader, FileWriter } = require('./files.tools');
+    const fileReader = new FileReader('./test.csv');
+    const writeStream = new FileWriter('result.csv');
+    const data = [];
+    await fileReader.start((line, i) => {
+        if (i !== 0) {
+            const [type,,,date,description,amount] = line.split(',');
+                data.push(`${date.split(' ')[0]};${computeType(type)};;${description};;${amount};;`);
+        }
+    });
+
+    writeStream.insertLines(data);
+    writeStream.close();
+})();
+
+//15-02-04;0;;;Some cash;-40,00;Bill:Withdrawal of cash;tag1 tag2
+//15-02-04;1;;;Internet DSL;-45,00;Inline service/Internet;tag2 my-tag3
+//date ; payment ; info ; payee ; memo ; amount ; category ; tags
